91d36f7a189ad9759c457d7ae3a191f1d3f467f9
thomasCoelho <56435612+thomasCoelho@users.noreply.github.com>
2020-07-01 16:07:45 +0200

    Vue.php

---
+++ Vue/vue.php
@@ -0,0 +1,33 @@
+<?php 
+class Vue {
+
+  private $fichier;
+  private $titre;
+
+  public function __construct($action) {
+    $this->fichier = "Vue/vue" . $action . ".php";
+  }
+
+
+  public function generer($donnees) {
+    $contenu = $this->genererFichier($this->fichier, $donnees);
+    // Génération du gabarit commun utilisant la partie spécifique
+    $vue = $this->genererFichier('Vue/gabarit.php',
+      array('titre' => $this->titre, 'contenu' => $contenu));
+    echo $vue;
+  }
+
+  private function genererFichier($fichier, $donnees) {
+    if (file_exists($fichier)) {
+      extract($donnees);
+      ob_start();
+      
+      require $fichier;
+
+      return ob_get_clean();
+    }
+    else {
+      throw new Exception("Fichier '$fichier' introuvable");
+    }
+  }
+}
