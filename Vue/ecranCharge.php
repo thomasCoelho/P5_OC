dfcfaa10038959fc5a1cd149c4d92ae56bc5be18
thomasCoelho <56435612+thomasCoelho@users.noreply.github.com>
2020-07-01 15:55:30 +0200

    Architecture principale
    Architecture principale + test ecran de charge/bienvenue

---
+++ Vue/ecranCharge.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Let's Play</title>
+<link rel="stylesheet" type="text/css" href="../Content/style.css">
+</head>
+
+<body>
+<svg id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 235.74 36"><path d="M313.15,296V260h8v32h12v4Z" stroke="rgba(255,153,117,0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/><path d="M341.17,296V260h20v4h-12v12h8v4h-8v12h12v4Z" stroke="rgba(255,153,117, 0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/><path d="M373.15,296V264h-8v-4h24v4h-8v32Z" stroke="rgba(255,153,117, 0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/><path d="M413.17,268h-4q-.53-4-3.79-4a4.49,4.49,0,0,0-3.17,1,4,4,0,0,0-1.06,3,4.11,4.11,0,0,0,1.18,2.81l8.47,9.53a8.35,8.35,0,0,1,2.35,5.64v2a7.73,7.73,0,0,1-2.35,5.61,7.64,7.64,0,0,1-5.64,2.37h-4c-2.66,0-4.65-.67-6-2s-2-3.33-2-6h4c0,.27,0,.49,0,.65a2.8,2.8,0,0,0,1.26,2.48,4.87,4.87,0,0,0,2.76.84,4,4,0,0,0,3.07-1,4.3,4.3,0,0,0,.94-3,4.12,4.12,0,0,0-1.18-2.81l-8.48-9.49a8.3,8.3,0,0,1-2.34-5.64v-2a8,8,0,0,1,8-8h4Q412.68,260,413.17,268Z" stroke="rgba(255,153,117, 0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/><path d="M432.91,296V260h16a8.06,8.06,0,0,1,8,8v4a8,8,0,0,1-8,8h-8v16Zm8-20h4a4.08,4.08,0,0,0,4-4v-4a3.8,3.8,0,0,0-1.19-2.8,3.88,3.88,0,0,0-2.82-1.17h-4Z" stroke="rgba(255,153,117,0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/><path d="M464.9,296V260h8v32h12v4Z" stroke="rgba(255,153,117,0.5)" stroke-width="1" transform="translate(-313.15 -260.01)"/><path d="M492.91,296V268a8,8,0,0,1,8-8h8a8.08,8.08,0,0,1,8,8v28h-8V280h-8v16Zm8-20h8v-8a3.8,3.8,0,0,0-1.19-2.8,4,4,0,0,0-5.65,0,3.79,3.79,0,0,0-1.18,2.8Z" stroke="rgba(255,153,117,0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/><path d="M532.88,296V280q-8-.92-8-8V260h8v12a3.89,3.89,0,0,0,1.18,2.82,4,4,0,0,0,6.85-2.82V260h8v12q0,7.08-8,8v16Z" stroke="rgba(255,153,117,0.5)" stroke-width="2" transform="translate(-313.15 -260.01)"/></svg>
+
+
+</body>
+
