ef1fd1bd5d52e5f0fdd61c80b9f19d368145339a
thomasCoelho <56435612+thomasCoelho@users.noreply.github.com>
2020-07-02 13:10:12 +0200

    Sign in user

--- Model/modeleUser.php
+++ Model/modeleUser.php
@@ -4,10 +4,15 @@ require_once 'Model/modele.php';
 
 class Session extends Modele {
 
-	public function adminIds($email){
+	public function userConnect($email){
 		$sql = "SELECT password_user FROM table_user WHERE email_user = ?";
 	    $idConnect = $this->executerRequete($sql, array($email));
 	    $varConnect = $idConnect->fetch();
 	    return $varConnect['password_user'];
+	}
+
+	public function userSignIn($email, $name, $password){
+		$sql = "INSERT INTO table_user(email_user, name_user, password_user) VALUES(?,?,?)";
+		$signIn = $this->executerRequete($sql, array($email, $name, $password));
 	}
 }
