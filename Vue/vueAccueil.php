233b2a74a521f7b0fee4dad144ddfb812dcf24fe
thomasCoelho <56435612+thomasCoelho@users.noreply.github.com>
2020-07-14 17:20:20 +0200

    Create vueAccueil.php

---
+++ Vue/vueAccueil.php
@@ -0,0 +1,28 @@
+		<link rel="stylesheet" type="text/css" href="../Content/css/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&family=Marvel:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+<header>
+	<ul id="header-accueil">
+		<li class="li-header-accueil">ACCUEIL</li>
+		<li class="li-header-accueil">JOUER</li>
+		<li class="li-header-accueil">CRÉER</li>
+		<li class="li-header-accueil" id="li-header-accueil-logo"><img src="../Content/images/logo.png"></li>
+		<li class="li-header-accueil" id="li-header-accueil-connect">SE CONNECTER</li>
+	</ul>
+</header>
+
+<section id="section-accueil">
+	<div class="accueil-flex-container">
+		<div id="accueil-yellow-rectangle" class="accueil-rectangle"></div>
+		<div id="accueil-purple-rectangle" class="accueil-rectangle"></div>
+	</div>
+	<div class="accueil-flex-container">
+		<div id="accueil-green-rectangle" class="accueil-rectangle"><P class="p-bouton-accueil">JOUER</P></div>
+		<div id="accueil-orange-rectangle" class="accueil-rectangle"><p class="p-bouton-accueil">CRÉER</p></div>
+	</div>
+	<h1 id="h1-accueil">VOUS NE POURREZ PLUS DIRE QUE VOUS NE LE SAVIEZ PAS</h1>
+	<img id="diego-accueil" src="../Content/images/DiegoAccueil.png">
+	<p id="p-accueil">Lancez vous dans une bataille de quiz de culture générale contre vos amis et votre famille pour un super moment de joie, ludique et amusant ! </p>
+</section>
+<footer>
+	<img class="logo-footer" src="../Content/images/logoFooter.svg">
+</footer>
